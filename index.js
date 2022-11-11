6991e60d6d13cb74bf089702bada5c1e8bd8661e
Nuno Moura <moura.abt@gmail.com>
2022-11-11 09:13:46 +0000

    Conditional code

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 export default {
-	fetch() {
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
+	fetch(request) {
+		if(request.url =="https://my-project.willsmithee.workers.dev/") {
+			return new Response('Hello worker!', {
+				headers: {
+					'content-type': 'text/plain',
+				},
+			});
+		}
+		else{
+			return new Response('Error Worker!', {
+				headers: {
+					'content-type': 'text/plain',
+				},
+			});
+		}
 	},
 };
