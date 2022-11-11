f347f5c6e45db4cead1c9e6b451f4b18fa5838be
Nuno Moura <moura.abt@gmail.com>
2022-11-11 10:00:27 +0000

    added POST method returning json

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 export default {
 	fetch(request) {
-		if(request.url =="https://cc1.cloudfeeling.workers.dev/test") {
-			return new Response('Hello worker!', {
-				headers: {
-					'content-type': 'text/plain',
-				},
-			});
+		// console.log(request.url);
+		// console.log(request.method);
+		if(request.method == "GET") {
+			if(request.url =="https://cc1.cloudfeeling.workers.dev/test") {
+				return new Response('Hello worker!', {
+					headers: {
+						'content-type': 'text/plain',
+					},
+				});
+			}
+			else{
+				return new Response('Error Worker!', {
+					headers: {
+						'content-type': 'text/plain',
+					},
+				});
+			}
 		}
-		else{
-			return new Response('Error Worker!', {
+		else if(request.method == "POST") {
+			// console.log("received POST request");
+			var reply = {
+				'message': 'You just POSTed something',
+				'method': 'POST'
+			}
+			return new Response(JSON.stringify(reply, null, 2), {
 				headers: {
-					'content-type': 'text/plain',
+					'content-type': 'application/json;charset=UTF-8',
 				},
 			});
 		}
